4c49191290fb4ab66ab81f99c153fb5ece9338a5
Triết Lê <trietle847@example.com>
2026-01-20 07:40:27 +0700

    update

--- Backend/src/main/java/com/CT553/demo/repository/BookingDetailRepository.java
+++ Backend/src/main/java/com/CT553/demo/repository/BookingDetailRepository.java
@@ -3,5 +3,9 @@ package com.CT553.demo.repository;
 import com.CT553.demo.entity.BookingDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookingDetailRepository extends JpaRepository<BookingDetail, Long> {
+    List<BookingDetail> findByCourt_Id(Long courtId);
 }
--- Backend/src/main/java/com/CT553/demo/repository/ProductRepository.java
+++ Backend/src/main/java/com/CT553/demo/repository/ProductRepository.java
@@ -1,7 +1,21 @@
 package com.CT553.demo.repository;
 
 import com.CT553.demo.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("""
+    SELECT p FROM Product p
+    WHERE (:categoryId IS NULL OR p.category.id = :categoryId)
+      AND (:keyword IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')))
+""")
+    Page<Product> search(
+            @Param("categoryId") Long categoryId,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
 }
