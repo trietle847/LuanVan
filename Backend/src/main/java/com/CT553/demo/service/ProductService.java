80fd6c35f2727a2faca404f5d2d2cc592ae07da0
Triết Lê <trietle847@example.com>
2026-01-20 07:37:22 +0700

    add getAllProduct

--- Backend/src/main/java/com/CT553/demo/service/ProductService.java
+++ Backend/src/main/java/com/CT553/demo/service/ProductService.java
@@ -6,7 +6,11 @@ import com.CT553.demo.entity.Product;
 import com.CT553.demo.mapper.ProductMapper;
 import com.CT553.demo.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,13 @@ public class ProductService {
     public ProductResponse createProduct(ProductRequest request) {
         Product product = productMapper.toEntity(request);
         return productMapper.toResponse(productRepository.save(product));
+    }
+    public Page<ProductResponse> getAllProduct(
+            Long categoryId,
+            String keyword,
+            Pageable pageable
+    ) {
+        return productRepository.search(categoryId, keyword, pageable)
+                .map(productMapper::toResponse);
     }
 }
