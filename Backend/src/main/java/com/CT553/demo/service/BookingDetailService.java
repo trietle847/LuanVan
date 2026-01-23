6011b5c76736774a91858333e88865c9785afbc9
Triết Lê <trietle847@example.com>
2026-01-23 12:06:06 +0700

    update create bookingDetail

--- Backend/src/main/java/com/CT553/demo/service/BookingDetailService.java
+++ Backend/src/main/java/com/CT553/demo/service/BookingDetailService.java
@@ -2,24 +2,24 @@ package com.CT553.demo.service;
 
 import com.CT553.demo.dto.request.BookingDetailRequest;
 import com.CT553.demo.dto.request.BookingProductRequest;
+//import com.CT553.demo.dto.request.BookingRequest;
 import com.CT553.demo.dto.response.BookingDetailResponse;
-import com.CT553.demo.dto.response.ProductResponse;
-import com.CT553.demo.entity.BookingDetail;
-import com.CT553.demo.entity.BookingProduct;
-import com.CT553.demo.entity.Court;
-import com.CT553.demo.entity.Product;
+import com.CT553.demo.dto.response.PriceConfigResponse;
+import com.CT553.demo.entity.*;
+import com.CT553.demo.entity.enums.BookingStatus;
 import com.CT553.demo.exception.ApiException;
 import com.CT553.demo.mapper.BookingDetailMapper;
 import com.CT553.demo.mapper.BookingProductMapper;
-import com.CT553.demo.mapper.ProductMapper;
-import com.CT553.demo.repository.BookingDetailRepository;
-import com.CT553.demo.repository.CourtRepository;
-import com.CT553.demo.repository.ProductRepository;
+import com.CT553.demo.mapper.PriceConfigMapper;
+import com.CT553.demo.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.time.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -32,33 +32,116 @@ public class BookingDetailService {
     private final BookingProductMapper bookingProductMapper;
     private final CourtRepository courtRepsitory;
     private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
+    private final UserRepository userRepository;
+    private final PriceConfigMapper priceConfigMapper;
 
-    public BookingDetailResponse createBookingDetail(BookingDetailRequest request) {
-        BookingDetail bookingDetail = bookingDetailMapper.toEntity(request);
+    private final PriceConfigRepsitory priceConfigRepsitory;
 
-        System.out.println(request);
+    @Transactional
+    public List<BookingDetailResponse> createBookingDetail(List<BookingDetailRequest> requests) {
+        List<BookingDetailResponse> responses = new ArrayList<>();
 
-        Court court = courtRepsitory.findById(request.getCourtId())
-                .orElseThrow(() -> new ApiException(BAD_REQUEST,"court not found"));
-        bookingDetail.setCourt(court);
+        for (BookingDetailRequest request: requests) {
+            BookingDetail bookingDetail = bookingDetailMapper.toEntity(request);
 
-        bookingDetail = bookingDetailRepository.save(bookingDetail);
+            Court court = courtRepsitory.findById(request.getCourtId())
+                    .orElseThrow(() -> new ApiException(BAD_REQUEST, "court not found"));
+            bookingDetail.setCourt(court);
 
+            bookingDetail = bookingDetailRepository.save(bookingDetail);
 
-        List<BookingProduct> bookingProducts = new ArrayList<>();
+            bookingDetail.setCreateAt(new Date());
+            bookingDetail.setStatus(BookingStatus.BOOK);
 
-        for (BookingProductRequest pr : request.getProducts()) {
-            BookingProduct bp = bookingProductMapper.toEntity(pr);
-            bp.setBookingDetail(bookingDetail);
-            bp.setProduct(
-                    productRepository.findById(pr.getProductId())
-                        .orElseThrow(() -> new ApiException(BAD_REQUEST,"product not found")));
-            bookingProducts.add(bp);
+//            thêm id người dùng
+            String username = SecurityContextHolder
+                    .getContext()
+                    .getAuthentication()
+                    .getName();
+
+            User user = userRepository.findByUsername(username)
+                    .orElseThrow(() ->
+                            new RuntimeException("user not find")
+                    );
+            bookingDetail.setUser(user);
+
+//        tính tiền sân theo giờ
+//  ------------------------------------------------------------------
+//      lấy ngày hiện tại kiểm tra thứ mấy
+            LocalDate localDate = request.getDate()
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate();
+            DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+            System.out.println("Thứ " + dayOfWeek);
+
+            int dayBit = switch (dayOfWeek) {
+                case MONDAY -> 1;      // T2
+                case TUESDAY -> 2;     // T3
+                case WEDNESDAY -> 4;   // T4
+                case THURSDAY -> 8;    // T5
+                case FRIDAY -> 16;     // T6
+                case SATURDAY -> 32;  // T7
+                case SUNDAY -> 64;    // CN
+            };
+
+//        lọc giá theo thứ
+            List<PriceConfig> configs = priceConfigRepsitory.findByTypeCourtId(court.getTypeCourt().getId())
+                    .stream().filter(pc -> (pc.getDays() & dayBit) != 0).toList();
+            System.out.println(" danh sách giá tiền theo ngày hôm đó " + priceConfigMapper.toResponse(configs));
+
+//        Tính tiền
+            LocalTime start = request.getStart();
+            LocalTime end = request.getEnd();
+
+            double feeCourt = 0;
+
+            for (PriceConfig pc : configs) {
+                LocalTime pcStart = pc.getStartTime();
+                LocalTime pcEnd = pc.getEndTime();
+
+                LocalTime from = start.isAfter(pcStart) ? start : pcStart;
+                LocalTime to = end.isBefore(pcEnd) ? end : pcEnd;
+
+                if (from.isBefore(to)) {
+                    long minutes = Duration.between(from, to).toMinutes();
+                    double hours = minutes / 60.0;
+                    feeCourt += hours * pc.getPrice();
+                }
+            }
+
+            System.out.println("tổng tiền " + feeCourt);
+//        System.out.println("thể loại sân "+ court.getTypeCourt().getId());
+//
+//        List<PriceConfig> priceConfigs = priceConfigRepsitory.findByTypeCourtId(court.getTypeCourt().getId());
+//
+//        System.out.println("thông tin giá tiền theo loại sân" + priceConfigMapper.toResponse((priceConfigs)));
+
+// -------------------------------------------------------------
+            List<BookingProduct> bookingProducts = new ArrayList<>();
+
+            Double feeService = 0.0;
+            for (BookingProductRequest pr : request.getProducts()) {
+                BookingProduct bp = bookingProductMapper.toEntity(pr);
+                Product product = productRepository.findById(pr.getProductId())
+                        .orElseThrow(() -> new ApiException(BAD_REQUEST, "product not found"));
+                feeService += product.getPrice() * pr.getQuantity();
+                System.out.println("tổng giá của sản phẩm " + product.getName() + " : " + product.getPrice() * pr.getQuantity());
+                bp.setBookingDetail(bookingDetail);
+//            bp.setProduct(
+//                    productRepository.findById(pr.getProductId())
+//                        .orElseThrow(() -> new ApiException(BAD_REQUEST,"product not found")));
+                bp.setProduct(product);
+                bookingProducts.add(bp);
+            }
+
+            bookingDetail.setTotalAmount(feeService + feeCourt);
+            bookingDetail.setBookingProducts(bookingProducts);
+            bookingDetailRepository.save(bookingDetail);
+            responses.add(bookingDetailMapper.toResponse(bookingDetail));
+//            return bookingDetailMapper.toResponse(bookingDetailRepository.save(bookingDetail));
         }
-
-        bookingDetail.setBookingProducts(bookingProducts);
-        return bookingDetailMapper.toResponse(bookingDetailRepository.save(bookingDetail));
+        return responses;
     }
 
     public List<BookingDetailResponse> getAllBookingDetail() {
@@ -140,4 +223,20 @@ public class BookingDetailService {
         }
         bookingDetailRepository.delete(bookingDetail);
     }
+
+    public List<BookingDetailResponse> getMyBookingDetail() {
+        String username = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ApiException(BAD_REQUEST, "User not found"));
+
+        return bookingDetailRepository.findByUserId(user.getId())
+                .stream()
+                .map(bookingDetailMapper::toResponse)
+                .toList();
+    }
+
 }
