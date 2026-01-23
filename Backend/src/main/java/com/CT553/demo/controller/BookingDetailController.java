1318979f7e35d5e48a164b3a23dd93d82f532ad9
Triết Lê <trietle847@example.com>
2026-01-23 11:40:43 +0700

    update create booking detail

--- Backend/src/main/java/com/CT553/demo/controller/BookingDetailController.java
+++ Backend/src/main/java/com/CT553/demo/controller/BookingDetailController.java
@@ -4,7 +4,7 @@ import com.CT553.demo.dto.request.BookingDetailRequest;
 import com.CT553.demo.dto.request.BookingProductRequest;
 import com.CT553.demo.dto.response.BookingDetailResponse;
 import com.CT553.demo.dto.response.BookingProductResponse;
-import com.CT553.demo.dto.response.BookingResponse;
+//import com.CT553.demo.dto.response.BookingResponse;
 import com.CT553.demo.service.BookingDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +18,7 @@ public class BookingDetailController {
     private BookingDetailService bookingDetailService;
 
     @PostMapping
-    public BookingDetailResponse createBookingDetail(@RequestBody BookingDetailRequest request) {
+    public List<BookingDetailResponse> createBookingDetail(@RequestBody List<BookingDetailRequest> request) {
         return bookingDetailService.createBookingDetail(request);
     }
 
@@ -28,8 +28,8 @@ public class BookingDetailController {
     }
 
     @GetMapping
-    public List<BookingDetailResponse> getAllBookingDetail(){
-        return bookingDetailService.getAllBookingDetail();
+    public List<BookingDetailResponse> getAllMyBookingDetail(){
+        return bookingDetailService.getMyBookingDetail();
     }
 
     @GetMapping("/by-court/{courtId}")
--- Backend/src/main/java/com/CT553/demo/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CT553.demo.repository;
-
-import com.CT553.demo.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
