ac998c801342654be0433d685f463f9cb6e5818b
Triết Lê <trietle847@example.com>
2026-02-02 07:22:40 +0700

    API for dashboard

--- Backend/src/main/java/com/CT553/demo/controller/StatisticController.java
+++ Backend/src/main/java/com/CT553/demo/controller/StatisticController.java
@@ -1,4 +1,57 @@
 package com.CT553.demo.controller;
 
+import com.CT553.demo.dto.response.statistics.*;
+import com.CT553.demo.service.InvoiceService;
+import com.CT553.demo.service.StatisticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/statistic")
 public class StatisticController {
+    @Autowired
+    private StatisticService statisticService;
+
+    @GetMapping("/invoice")
+    public InvoiceStatistic statisticInvoiceByMonth(
+            @RequestParam int month,
+            @RequestParam int year) {
+
+        return statisticService.getStatisticByMonth(month, year);
+    }
+
+    @GetMapping("/product-usage")
+    public List<ProductUsageStatistic>  statisticProduct(
+            @RequestParam int month,
+            @RequestParam int year) {
+        return statisticService.getProductUsage(month, year);
+    }
+
+    @GetMapping("/court-usage")
+    public List<CourtUsageStatistic> statisticCourtUsage(
+            @RequestParam int month,
+            @RequestParam int year) {
+        return statisticService.getCourtUsage(month,year);
+    }
+
+    @GetMapping("/booking")
+    public List<BookingStatistic> statisticBooking(
+            @RequestParam int month,
+            @RequestParam int year
+    ) {
+        return statisticService.getBookingStatisticByDay(month, year);
+    }
+
+    @GetMapping("/type-usage")
+    public List<TypeCourtStatistic> getTypeCourtUsage(
+            @RequestParam int month,
+            @RequestParam int year
+    ) {
+        return statisticService.getTypeCourtUsage(month, year);
+    }
 }
--- Backend/src/main/java/com/CT553/demo/service/StatisticService.java
+++ Backend/src/main/java/com/CT553/demo/service/StatisticService.java
@@ -1,4 +1,110 @@
 package com.CT553.demo.service;
 
+import com.CT553.demo.dto.response.statistics.*;
+import com.CT553.demo.entity.Court;
+import com.CT553.demo.exception.ApiException;
+import com.CT553.demo.repository.BookingDetailRepository;
+import com.CT553.demo.repository.BookingProductRepository;
+import com.CT553.demo.repository.CourtRepository;
+import com.CT553.demo.repository.InvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Service
+@RequiredArgsConstructor
 public class StatisticService {
+    private final InvoiceRepository invoiceRepository;
+    private final BookingProductRepository bookingProductRepository;
+    private final BookingDetailRepository bookingDetailRepository;
+    private final CourtRepository courtRepository;
+
+    public InvoiceStatistic getStatisticByMonth(int month, int year) {
+        List<Object[]> results = invoiceRepository.statisticByMonth(month, year);
+
+        InvoiceStatistic statistic = new InvoiceStatistic();
+        statistic.setMonth(month);
+        statistic.setYear(year);
+
+        if (!results.isEmpty()) {
+            Object[] row = results.get(0);
+
+            statistic.setTotalInvoice(((Number) row[0]).longValue());
+            statistic.setTotalRevenue(row[1] != null
+                    ? ((Number) row[1]).doubleValue()
+                    : 0);
+        } else {
+            statistic.setTotalInvoice(0L);
+            statistic.setTotalRevenue(0);
+        }
+
+        return statistic;
+    }
+
+    public List<ProductUsageStatistic> getProductUsage(int month, int year) {
+        List<Object[]> results = bookingProductRepository.statisticProductByMonth(month, year);
+
+        List<ProductUsageStatistic> productUsageStatistics = new ArrayList<>();
+        for (Object[] row : results) {
+            ProductUsageStatistic dto = new ProductUsageStatistic();
+            dto.setProductId(((Number) row[0]).longValue());
+            dto.setName((String) row[1]);
+            dto.setTotalQuantity(((Number) row[2]).longValue());
+            dto.setMonth(month);
+            dto.setYear(year);
+            productUsageStatistics.add(dto);
+        }
+        return productUsageStatistics;
+    }
+
+    public List<CourtUsageStatistic> getCourtUsage(int month, int year) {
+        List<Object[]> results = bookingDetailRepository.statisticCourtUsage(month, year);
+
+        List<CourtUsageStatistic> courtUsageStatistics = new ArrayList<>();
+
+        for (Object[] row: results) {
+            CourtUsageStatistic dto = new CourtUsageStatistic();
+            dto.setCourtId(((Number) row[0]).longValue());
+            dto.setCourtName((String) row[1]);
+            dto.setTotalBooking(((Number) row[2]).longValue());
+            dto.setMonth(month);
+            dto.setYear(year);
+            courtUsageStatistics.add(dto);
+        }
+        return  courtUsageStatistics;
+    }
+
+    public List<BookingStatistic> getBookingStatisticByDay(int month, int year) {
+        List<Object[]> results = bookingDetailRepository.statisticBookingByDay(month, year);
+        List<BookingStatistic> bookingStatistics = new ArrayList<>();
+
+        for (Object[] row: results) {
+            BookingStatistic dto = new BookingStatistic();
+            dto.setDay(((Number)row[0]).intValue());
+            dto.setTotalBooking(((Number)row[1]).intValue());
+            bookingStatistics.add(dto);
+        }
+
+        return bookingStatistics;
+    }
+
+    public List<TypeCourtStatistic> getTypeCourtUsage(int month, int year) {
+        List<Object[]> results = bookingDetailRepository.statisticCourtTypeUsage(month, year);
+        List<TypeCourtStatistic> typeCourtStatistics = new ArrayList<>();
+
+        for (Object[] row: results) {
+            TypeCourtStatistic dto = new TypeCourtStatistic();
+            dto.setTypeCourtId(((Number) row[0]).intValue());
+            dto.setTypeCourtName((String) row[1]);
+            dto.setTotalBooking(((Number) row[2]).intValue());
+            dto.setMonth(month);
+            dto.setYear(year);
+            typeCourtStatistics.add(dto);
+        }
+        return typeCourtStatistics;
+    }
 }
