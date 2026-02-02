26a0c81c367bb0064e7ad59067ba45edf591631d
Triết Lê <trietle847@example.com>
2026-02-02 07:25:05 +0700

    update

--- Backend/src/main/java/com/CT553/demo/mapper/InvoiceMapper.java
+++ Backend/src/main/java/com/CT553/demo/mapper/InvoiceMapper.java
@@ -1,4 +1,19 @@
 package com.CT553.demo.mapper;
 
+import com.CT553.demo.dto.request.InvoiceRequest;
+import com.CT553.demo.dto.response.InvoiceResponse;
+import com.CT553.demo.entity.Invoice;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring", uses = {BookingDetailMapper.class})
 public interface InvoiceMapper {
+    @Mapping(target = "id", ignore = true)
+    Invoice toEntity(InvoiceRequest request);
+
+    @Mapping(source = "bookingDetails", target = "bookingDetailResponses")
+    InvoiceResponse toResponse(Invoice entity);
+
+    void updateEntityFromRequest(InvoiceRequest request, @MappingTarget Invoice entity);
 }
