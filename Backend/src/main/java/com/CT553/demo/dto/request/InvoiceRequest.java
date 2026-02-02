8038137f71f595a4b3f30d460453bb1df67152a9
Triết Lê <trietle847@example.com>
2026-02-02 07:25:51 +0700

    update request dto

--- Backend/src/main/java/com/CT553/demo/dto/request/InvoiceRequest.java
+++ Backend/src/main/java/com/CT553/demo/dto/request/InvoiceRequest.java
@@ -1,4 +1,11 @@
 package com.CT553.demo.dto.request;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class InvoiceRequest {
+    private String name;
+    private List<Long> bookingDetailIds;
 }
